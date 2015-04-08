b4542075755ed0950da0a4019950a0433624c137
Tanjo, Hiroyuki <expheno@gmail.com>
2015-04-08 12:31:27 +0900

    Add download buttons

--- example/app.js
+++ example/app.js
@@ -39,7 +39,7 @@
     "required": ["name"]
   };
   var source = '<h1>Hello, {{type}}{{name}}!</h1>\n' +
-               '<h2>Friends:</h2>' +
+               '<h2>Friends:</h2>\n' +
                '<ul>\n' +
                '{{#each friends}}\n' +
                '<li>{{name}}</li>\n' +
@@ -52,35 +52,48 @@
   .module('webpageToolkit', ['schemaForm'])
   .controller('FormController', [
     '$scope',
-    '$sce',
-    function ($scope, $sce) {
+    function ($scope) {
 
       $scope.schema = schema;
       $scope.model = {};
 
-      $scope.form = [
-        '*',
-        {
-          type: 'submit',
-          title: 'Save'
-        }
-      ];
+      $scope.form = ['*'];
 
-      $scope.submit = function (form) {
-        $scope.$broadcast('schemaFormValidate');
-        if (form.$valid) {
-          console.log($scope.model);
-        }
+      $scope.getHtml = function () {
+        return template($scope.model);
       };
 
-      $scope.preview = function () {
-        var html = template($scope.model);
-        viewport.html(html);
+      $scope.getJson = function () {
+        return JSON.stringify($scope.model, null, '  ');
+      };
+
+      $scope.render = function () {
+        viewport.html($scope.getHtml());
+      };
+
+      $scope.downloadHtml = function () {
+        $scope.$broadcast('schemaFormValidate');
+        if (!$scope.appForm.$valid) {
+          alert('Invalid form data');
+          return;
+        }
+        $scope.download('index.html', $scope.getHtml());
+      };
+
+      $scope.downloadJson = function () {
+        $scope.download('data.json', $scope.getJson());
+      };
+
+      $scope.download = function (filename, text) {
+        var pom = document.createElement('a');
+        pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        pom.setAttribute('download', filename);
+        pom.click();
       };
 
       $scope.$watch('model', _.debounce(function () {
         $scope.$apply(function () {
-          $scope.preview();
+          $scope.render();
         });
       }, 500), true);
 
